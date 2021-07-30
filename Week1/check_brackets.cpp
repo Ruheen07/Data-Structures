5a4deb4b491169f0bd96ab3646ef454ef423227f
Ruheen07 <86929491+Ruheen07@users.noreply.github.com>
2021-07-30 17:08:17 +0530

    Update check_brackets.cpp

--- Week1/check_brackets.cpp
+++ Week1/check_brackets.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 #include <string>
+#include <stack>
 #include <bits/stdc++.h>
 using namespace std;
 
 
 
 int main(){
-    stack<char> f;
+    stack <char> f;
     string s;
+    int i;
     cin>>s;
-    for(int i=0; i<s.length(); i++){
+    for(i=0; i<s.length(); i++){
+        if(s.length()==1){
+            cout<<"1";
+            
+        }
         if(s[i]=='(' || s[i]=='{' || s[i]=='['){
             f.push(s[i]);
             continue;
         }
-        if(s[i]==')' || s[i]=='}' || s[i]==']'){
-            f.push(s[i]);
+        
+        else if((f.top()=='{' && s[i]=='}') || (f.top()=='(' && s[i]==')') || (f.top()=='[' && s[i]==']')){
+            f.pop();
+            continue;
         }
+        
+        else if((f.top()=='{' && s[i]!='}') || (f.top()=='(' && s[i]!=')') || (f.top()=='[' && s[i]!=']')){
+            break;
+        }
+        
+        else
+            continue;
+    }   
+    if(f.empty()){
+        cout<<"SUCCESS";
     }
-    if(stack.empty())
-        cout>>"Success";
-    else
-        cout>>f.size();
+    if(!f.empty()){
+        cout<<i+1;
+    }
 }
