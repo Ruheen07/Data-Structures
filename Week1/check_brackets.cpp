993c9993de6d909ebfca0289f4bbebf1d81f5c3a
Ruheen07 <86929491+Ruheen07@users.noreply.github.com>
2021-07-31 14:31:40 +0530

    Update check_brackets.cpp

--- Week1/check_brackets.cpp
+++ Week1/check_brackets.cpp
@@ -1,42 +1,72 @@
 #include <iostream>
-#include <string>
 #include <stack>
-#include <bits/stdc++.h>
-using namespace std;
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
+struct Bracket {
+    Bracket(char type, int position):
+        type(type),
+        position(position)
+    {}
 
-
-int main(){
-    stack <char> f;
-    string s;
-    int i;
-    cin>>s;
-    for(i=0; i<s.length(); i++){
-        if(s.length()==1){
-            cout<<"1";
-            
-        }
-        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
-            f.push(s[i]);
-            continue;
-        }
-        
-        else if(!f.empty() && ((f.top()=='{' && s[i]=='}') || (f.top()=='(' && s[i]==')') || (f.top()=='[' && s[i]==']'))){
-            f.pop();
-            continue;
-        }
-        
-        else if(!f.empty() && ((f.top()=='{' && (s[i]==']' || s[i]==')')) || (f.top()=='(' && (s[i]=='}' || s[i]==']')) || (f.top()=='[' && (s[i]=='}' || s[i]==')')))){
-            break;
-        }
-        
-        else
-            continue;
-    }   
-    if(f.empty()){
-        cout<<"SUCCESS";
+    bool Matchc(char c) {
+        if (type == '[' && c == ']')
+            return true;
+        if (type == '{' && c == '}')
+            return true;
+        if (type == '(' && c == ')')
+            return true;
+        return false;
     }
-    if(!f.empty()){
-        cout<<i+1;
+
+    char type;
+    int position;
+};
+
+int main() {
+    std::string text;
+    getline(std::cin, text);
+	
+	int error_pos = 0;
+
+    std::stack <Bracket> opening_brackets_stack;
+    for (int position = 0; position < text.length(); ++position) {
+        char next = text[position];
+
+        if (next == '(' || next == '[' || next == '{') {
+            // Process opening bracket, write your code here
+			Bracket b(next, position+1);
+			opening_brackets_stack.push(b);
+        }
+		
+        if (next == ')' || next == ']' || next == '}') {
+            // Process closing bracket, write your code here
+			if(opening_brackets_stack.empty()) {
+				error_pos = position + 1;
+				break;
+			}
+			Bracket top = opening_brackets_stack.top();
+			opening_brackets_stack.pop();
+			if(!top.Matchc(next)) {
+				error_pos = position + 1;
+				break;
+			}
+        }
     }
+
+    // Printing answer, write your code here
+	if(error_pos==0 && opening_brackets_stack.empty())
+		std::cout << "Success";
+	else {
+		if(error_pos == 0) {
+			while(opening_brackets_stack.size()>1)
+				opening_brackets_stack.pop();
+			error_pos = opening_brackets_stack.top().position;
+		}
+		std::cout << error_pos;
+	}
+
+    return 0;
 }
